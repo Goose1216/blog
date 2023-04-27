f1977bbecf5faef4ae2d4b1023f1a27acbcd58fa
Goose1216 <leha.porublev@mail.ru>
2023-04-27 20:34:45 +0300

    Authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,4 +126,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='home'
